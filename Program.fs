e523d453c4aea609cdeddf767958ef2075fa768b
thed24 <thed24@gmail.com>
2023-06-09 01:01:02 +1000

    I fought the type system and won

---
+++ Program.fs
@@ -0,0 +1,24 @@
+﻿module Program
+
+open System.IO
+open Scanner
+
+let readFileFromPath (path: string) =
+    seq {
+        use reader = new StreamReader(path)
+
+        while not reader.EndOfStream do
+            yield reader.ReadLine()
+    }
+
+let joinLines (input: seq<string>) =
+    input |> Seq.fold (fun acc line -> acc + line) ""
+
+[<EntryPoint>]
+let main args =
+    let parseVar = Parser.fromString "var"
+    let scanned = readFileFromPath "input.txt" |> joinLines |> parseVar.run
+    
+    fprintf stdout "%A" scanned
+
+    0
